6c3e811ee0e83f0e926732a9c751e934f05369fb
ycy <568082704@qq.com>
2019-08-04 11:55:12 +0800

    加提交功能

---
+++ js/message.js
@@ -0,0 +1,131 @@
+var APP_ID = 'fjlraHVDQ4tHK3Fzksbac1Bo-gzGzoHsz';
+var APP_KEY = 'a3RTalL5U6LzNcjedHee9KFL';
+
+AV.init({
+  appId: APP_ID,
+  appKey: APP_KEY
+});
+console.log('运行到这里没有报错')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// //创建TestObject表
+// var X = AV.Object.extend('Frank2');
+// //在表中创建一行数据
+// var o = new X();
+// //数据内容是word：‘Hello world！’保存
+// //如果把保存成功就运行，console.log()
+
+// o.set('xxxxxx', 'ff world!');
+// o.save().then(function (o) {
+//   console.log(o)
+//   console.log('保存成功。')
+// })
